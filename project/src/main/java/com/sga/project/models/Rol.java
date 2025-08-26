b832174db4c0b519e3711c843549d3c89f58831e
Leonardo <leonardobautista67@gmail.com>
2025-08-26 17:19:18 -0500

    commit leo empezando

--- project/src/main/java/com/sga/project/models/Rol.java
+++ project/src/main/java/com/sga/project/models/Rol.java
@@ -1,5 +1,24 @@
 package com.sga.project.models;
 
-public class Rol {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table ( name = "rol")
+public class Rol {
+    @Id
+    @Column (name = "id_rol")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_rol;
+    private String nomRol;
 }
--- project/src/main/java/com/sga/project/models/Usuario.java
+++ project/src/main/java/com/sga/project/models/Usuario.java
@@ -1,5 +1,51 @@
 package com.sga.project.models;
 
-public class Usuario {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table (name = "usuario")
+public class Usuario {
+    @Id
+    @Column (name = "num_doc")
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer num_doc;
+    @Column(length = 18)
+    private String nom1;
+    @Column(length = 18)
+    private String nom2;
+    @Column(length = 20)
+    private String ape2;
+    @Column(length = 20)
+    private String ape3;
+    @Column(length = 35)
+    private String direccion;
+    private Integer numTel;
+    @Column(length = 100)
+    private String correoElec;
+    @Column(length = 200)
+    private String contraseña;
+    @ManyToOne(optional = false)
+    @JoinColumn( name = "id_barrio", nullable = false, foreignKey =  @ForeignKey(name = "FK_usuario_barrio"))
+    private Barrio barrio;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_tipoDoc", nullable = false, foreignKey = @ForeignKey(name = "FK_usuarios_documento"))
+    private TipoDoc tipoDoc;
+    @ManyToOne (optional = false)
+    @JoinColumn(name = "id_rol", nullable = false, foreignKey = @ForeignKey(name = "FK_usuarios_rol"))
+    private Rol rol;
 }
